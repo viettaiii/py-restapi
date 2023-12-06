c79daca18c4fccb51b7c0507570e6f4d0d5c090b
Viet Tai <viettaixca123@gmail.com>
2023-12-06 20:13:09 +0700

    Token Authentication

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
-
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
+    path('auth/', obtain_auth_token),
     path('', views.api_home), ## localhost:8000/api/
 ]
 
--- py_client/list.py
+++ py_client/list.py
@@ -1,10 +1,15 @@
 import requests
+from getpass import getpass
 
+auth_endpoint = "http://localhost:8000/api/auth/"
+
+username = 'root'
+password = getpass()
+auth_response = requests.post(auth_endpoint, json={'username':username, 'password':password})
+headers = {'Authorization': f'Bearer {auth_response.json()['token']}'}
 
 endpoint = "http://localhost:8000/api/products/"
-
-
-response = requests.get(endpoint  ) 
+response = requests.get(endpoint , headers =headers  ) 
 # print(response.headers)
 # print(response.text)
 print(response.json())
