22962fe588f61e800961fd8db395c32b7420a66c
Viet Tai <viettaixca123@gmail.com>
2023-12-06 13:33:36 +0700

    Rest Framework View & Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,20 @@
 from django.http import JsonResponse,HttpResponse
 from django.forms.models import model_to_dict
 from django.core.serializers import serialize
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 import json
 
 from products.models import Product
 
+
+@api_view(['POST'])
 def api_home(request, *args , **kwargs):
-    model_data = Product.objects.all()
+    model_data = Product.objects.all().order_by('?').first()
     data = {}
     if model_data:
-        data = serialize('json' , model_data)
+        data = model_to_dict(model_data)
   
-    return JsonResponse(data , safe=False)
+    return Response(data )
